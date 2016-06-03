43345ab62cc8fdb58a23765619f57f467556677b
Artem Sviridov <SvyrydovArtem@gmail.com>
2016-06-03 19:51:36 +0300

    first commit

---
+++ meadowlark.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Sviridov on 03.06.2016.
+ */
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+];
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+app.get('/about', function(req, res){
+    var randomFortune =fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', { fortune: randomFortune });
+});
+// custom 404 page
+app.use(function(req, res, next){
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(req, res){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'),function () {
+    console.log('Express started on http://localhost:'+ app.get('port')+ '; press Ctrl+C to terminate');
+});
